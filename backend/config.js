8fce281c5fdc0a8dc13f2355047d0878f2e6b280
Aayush Gupta <aayushgupta69official@gmail.com>
2024-03-29 21:12:48 +0530

    Added cors, body parser and jsonwebtoken

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "aayushsecret";
+
+module.exports = JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require("express");
 const {connectToMongoDB} = require("./database");
 const rootRouter = require("./routes/index");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/v1", rootRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log("Started listening on port!");
+    console.log("Server listening on port " + process.env.PORT);
 });
