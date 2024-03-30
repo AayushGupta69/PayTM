91a56b279bc1470883a7f3c713abc2ef129a0b51
Aayush Gupta <aayushgupta69official@gmail.com>
2024-03-30 21:39:20 +0530

    Added routing to frontend react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Dashboard} from "./pages/Dashboard.jsx";
+import {SendMoney} from './pages/SendMoney.jsx';
+import {Signin} from './pages/Signin.jsx';
+import {Signup} from "./pages/Signup.jsx";
 
 function App() {
 	return (
 		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/signup" element={<Signup />}/>
+					<Route path="/signin" element={<Signin />}/>
+					<Route path="/dashboard" element={<Dashboard />}/>
+					<Route path="/send" element={<SendMoney />}/>
+				</Routes>
+			</BrowserRouter>
 		</>
 	)
 }
